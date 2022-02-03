77300f077836dbcc27243e2a953ee2a3d5905cf6
Simon Klein <simon.klein@cloudogu.com>
2022-02-03 12:06:04 +0100

    #26 Fix apply script

--- dev/apply.sh
+++ dev/apply.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 DOGU="${1}"
 
+echo "Plugin-Dir: ${PLUGIN_DIR}"
+
 rm -rf ./redmine_cas
 ../bundle/bundle_plugin.rb
-docker exec -it "${DOGU}" rm -rf "/usr/share/webapps/${DOGU}/plugins/redmine_cas"
-docker cp ./redmine_cas "${DOGU}:/usr/share/webapps/${DOGU}/plugins/"
+if [ "${DOGU}" == "redmine" ]; then
+  docker exec -it "${DOGU}" rm -rf "/usr/share/webapps/${DOGU}/defaultPlugins/redmine_cas"
+  docker exec -it "${DOGU}" rm -rf "/usr/share/webapps/${DOGU}/plugins/redmine_cas"
+  docker cp ./redmine_cas "${DOGU}:/usr/share/webapps/${DOGU}/defaultPlugins/"
+  docker cp ./redmine_cas "${DOGU}:/usr/share/webapps/${DOGU}/plugins/"
+else
+  docker exec -it "${DOGU}" rm -rf "/usr/share/webapps/${DOGU}/plugins/redmine_cas"
+  docker cp ./redmine_cas "${DOGU}:/usr/share/webapps/${DOGU}/plugins/"
+fi
 docker restart "${DOGU}"
 tail -f "/var/log/docker/${DOGU}.log"
