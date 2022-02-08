e761d084020ba5b7b035ad22be7e053734462594
Christoph Wolfes <christoph.wolfes@cloudogu.com>
2022-02-08 15:28:29 +0100

    #26 fix reading of bool settings

---
+++ src/redmine_cas/init.rb
@@ -0,0 +1,33 @@
+require 'redmine'
+require 'redmine_cas'
+require 'redmine_cas/application_controller_patch'
+require 'redmine_cas/account_controller_patch'
+
+require_dependency 'redmine_cas_hook_listener'
+
+Redmine::Plugin.register :redmine_cas do
+  name 'Redmine CAS plugin'
+  author 'Robert Auer (Cloudogu GmbH)'
+  description 'Plugin to CASify your Redmine installation.'
+  version '1.5.2'
+  url 'https://github.com/cloudogu/redmine_cas'
+
+  settings :default => {
+    'enabled' => 1,
+    'attributes_mapping' => 'firstname=givenName&lastname=surname&mail=mail&login=username&allgroups=allgroups',
+    'redmine_fqdn' => '192.168.56.2',
+    'cas_fqdn' => '192.168.56.2',
+    'cas_relative_url' => '/cas',
+    'local_users_enabled' => 1,
+    'admin_group' => 'admin',
+  }, :partial => 'redmine_cas/settings'
+
+  Rails.configuration.to_prepare do
+    ApplicationController.send(:include, RedmineCAS::ApplicationControllerPatch)
+    AccountController.send(:include, RedmineCAS::AccountControllerPatch)
+    User.send(:include, RedmineCAS::UserPatch)
+  end
+  ActionDispatch::Callbacks.before do
+    RedmineCAS.setup!
+  end
+end
