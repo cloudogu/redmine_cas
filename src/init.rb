06ba08d6bd7fa02a435ada506b6bbbbf97bd6c03
Simon Klein <simon.klein@cloudogu.com>
2022-10-11 13:48:36 +0200

    change version

--- src/init.rb
+++ src/init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_cas do
   name 'Redmine CAS plugin'
   author 'Robert Auer (Cloudogu GmbH)'
   description 'Plugin to CASify your Redmine installation.'
-  version '2.0.0'
+  version 'ipv6'
   url 'https://github.com/cloudogu/redmine_cas'
 
   settings :default => {
