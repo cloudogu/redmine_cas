c4fcbbe0d92a96cf7efe1d45c1bc2327537d8e89
Simon Klein <simon.klein-extern@cloudogu.com>
2023-07-04 09:56:42 +0200

    #31 Correct import

--- src/after_init.rb
+++ src/after_init.rb
@@ -3,12 +3,16 @@ RedmineExtensions::Reloader.to_prepare do
   require 'redmine_cas/application_controller_patch'
   require 'redmine_cas/account_controller_patch'
 
+  require 'redmine_cas/redmine_cas_hook_listener'
+
+
   ApplicationController.send(:include, RedmineCas::ApplicationControllerPatch)
   AccountController.send(:include, RedmineCas::AccountControllerPatch)
   User.send(:include, RedmineCas::UserPatch)
 
-  ActionDispatch::Callbacks.before do
-    RedmineCas.setup!
-  end
-
 end
+
+ActionDispatch::Callbacks.before do
+  RedmineCas.setup!
+end
+
