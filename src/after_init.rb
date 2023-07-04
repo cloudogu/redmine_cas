6ab2d69dfe717ce7f4ca6db727fdf6a753e10d98
Simon Klein <simon.klein-extern@cloudogu.com>
2023-07-04 10:02:35 +0200

    #31 Remove imports

--- src/after_init.rb
+++ src/after_init.rb
@@ -1,15 +1,7 @@
 RedmineExtensions::Reloader.to_prepare do
-  require 'redmine_cas'
-  require 'redmine_cas/application_controller_patch'
-  require 'redmine_cas/account_controller_patch'
-
-  require 'redmine_cas/redmine_cas_hook_listener'
-
-
   ApplicationController.send(:include, RedmineCas::ApplicationControllerPatch)
   AccountController.send(:include, RedmineCas::AccountControllerPatch)
   User.send(:include, RedmineCas::UserPatch)
-
 end
 
 ActionDispatch::Callbacks.before do
